b944d6561acfa475a8f0b240e0d1e6a0c325acc1
boconik <boconik123@gmail.com>
2023-02-26 13:03:58 +0100

    minter dapp test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x724fe5Ed59bAB88BEBf42C6b8bC77e16B7C9B891"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
