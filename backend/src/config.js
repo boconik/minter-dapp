26004f7ec935875c33e28b9209d69e3b91a5b97e
boconik <boconik123@gmail.com>
2023-03-05 10:29:47 +0100

    fyeah

--- backend/src/config.js
+++ backend/src/config.js
@@ -72,7 +72,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = ["0xCCB7deEAb2595230B80bBa0A6f9705Db1B0Cc2ee","0x3a5F50faCCF81b1096A013e0418700233F39145B","0xAdc167288129639e3C3339b33F0445A6Bd3F5344","0x1A108251508b57cc55e54bf75a983948E2fC0021"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "0x5C79eddefaC83321CDfdeAb9Cd1941B084FFE284"; // If you want to manually include it
+let CONTRACT_ADDRESS = "YOUR_CONTRACT"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
